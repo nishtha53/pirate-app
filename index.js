d8e783d3834c30e710543bc0743f0cbeced480e0
nishtha53 <68805896+nishtha53@users.noreply.github.com>
2022-07-31 22:36:57 +0530

    Pirate app competed

---
+++ index.js
@@ -0,0 +1,26 @@
+var buttonTranslate = document.querySelector("#btn-pirate-translator");
+var txt = document.querySelector(".txt-input-pirate");
+var outputdiv = document.querySelector("#output-pirate");
+
+var serverUrl = "https://api.funtranslations.com/translate/pirate.json";
+
+function getServerURL(text) {
+    return serverUrl + "?text=" + text;
+}
+
+function errorHandler(error) {
+    console.log("Error is :", error);
+    alert("Try Again!")
+}
+
+function clickHandler(){
+    var inputtxt = txt.value;
+    fetch(getServerURL(inputtxt))
+    .then(response => response.json())
+    .then(json=>{
+        var translatedText = json.contents.translated;
+        outputdiv.innerText = translatedText;
+    })   
+}
+
+buttonTranslate.addEventListener("click", clickHandler);
